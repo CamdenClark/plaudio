c50ffa4a887ca45f362d13f8203be173855082d2
Camden <camdenaws@gmail.com>
2020-05-19 23:49:17 -0700

    We can synthesize some text!

--- server.js
+++ server.js
@@ -1,12 +1,42 @@
 const express = require('express');
+const textToSpeech = require('@google-cloud/text-to-speech');
+const { Storage } = require('@google-cloud/storage');
+
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello from App Engine!');
 });
 
-app.get('/posts', (req, res) => {
-    res.send('These are some posts');
+app.get('/posts', async (req, res) => {
+    const client = new textToSpeech.TextToSpeechClient();
+    const storage = new Storage();
+    const request = {
+        input: { text: 'Google cloud is synthesizing some text.' },
+        voice: { languageCode: 'en-US', ssmlGender: 'NEUTRAL' },
+        audioConfig: { audioEncoding: 'MP3' },
+    };
+
+    try {
+        const [response] = await client.synthesizeSpeech(request);
+        const blob = storage.bucket("homophone-test").file('google-cloud-test.mp3');
+        const blobStream = blob.createWriteStream();
+
+        blobStream.on('error', (err) => {
+            next(err);
+            console.log(err);
+        });
+
+        blobStream.on('finish', () => {
+            const publicUrl = 'https://storage.googleapis.com/homophone-test/' + blob.name;
+            res.status(200).send(publicUrl);
+        });
+
+        blobStream.end(response.audioContent);
+    }
+    catch (error) {
+        console.log(error.message);
+    }
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
