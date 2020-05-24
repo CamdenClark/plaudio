a975bcc1453f601581891ca0e0dd2a6c64babfff
Camden <camdenaws@gmail.com>
2020-05-24 00:36:58 -0700

    Good checkpoint for computing votes!

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const Firestore = require('@google-cloud/firestore');
 const { PubSub } = require('@google-cloud/pubsub');
+const Firestore = require('@google-cloud/firestore');
 
 const pubsub = new PubSub();
 const TOPIC_NAME = "text-to-speech";
 const topic = pubsub.topic(TOPIC_NAME);
+const voteTopic = pubsub.topic("vote-trigger");
 
 const db = new Firestore({
     projectid: 'homophone',
@@ -22,54 +23,42 @@ app.get('/', (req, res) => {
 app.get('/sounds', async (req, res) => {
     const query = db
         .collection('sounds')
-        //.where('createdAt', '>', new Date(Date.now() - (1000 * 60 * 60 * 24)))
         .orderBy('computedScore', 'desc').limit(10);
     const sounds = await query.get();
     const { docs } = sounds;
-    res.send(docs.map(doc => doc.data()).map(doc => { doc["createdAt"] = doc["createdAt"].seconds; return doc }));
-});
-
-app.get('/compute', async (req, res) => {
-    const query = db.collection('sounds')
-        .where('createdAt', '>', new Date(Date.now() - (1000 * 60 * 60 * 24)));
-    const sounds = await query.get();
-    const { docs } = sounds;
-    for (let doc of docs) {
-        const { createdAt, score } = doc.data();
-        console.log(createdAt)
-        console.log(`Computed document with score ${score} and created at ${createdAt.seconds}`);
-        const tS = createdAt.seconds - 1590285563;
-        const y = score > 0 ? 1 : score === 0 ? 0 : -1;
-        const z = Math.abs(score) < 1 ? 1 : Math.abs(score);
-        const newComputedScore = Math.log10(z) + ((y * tS) / 45000);
-        console.log(`New computed score ${newComputedScore}`);
-        await db.doc(doc.ref.path).update({
-            computedScore: newComputedScore
-        });
-    }
-    res.sendStatus(200);
+    res.send(
+        docs.map(doc => doc.data())
+            .map(doc => { doc["createdAt"] = doc["createdAt"].seconds; return doc }));
 });
 
 app.post('/sounds', async (req, res) => {
-    const { id, text, username } = req.body;
-    console.log(`Creating sound with id ${id}, username ${username}, and text ${text}.`);
-    const document = db.doc(`sounds/${id}`);
+    const { soundId, text, userId } = req.body;
+    console.log(`Creating sound with id ${soundId}, user id ${userId}, and text ${text}.`);
+    const document = db.doc(`sounds/${soundId}`);
     await document.set({
-        id: id,
+        soundId,
+        userId,
         text: text,
         createdAt: new Date(Date.now()),
-        username: username,
-        score: 1
+        score: 0,
+        computedScore: 0
     });
 
-    await db.doc(`votes/${id}_${username}`).set({ vote: 1 });
+    await db.doc(`sounds/${soundId}/votes/${userId}`).set({ vote: 1 });
 
     const buffer = Buffer.from(JSON.stringify({
-        text: text,
-        id: id,
-        username: username
+        text,
+        id: soundId,
+        username: userId
     }));
     topic.publish(buffer);
+
+    const voteBuffer = Buffer.from(JSON.stringify({
+        soundId,
+        scoreDelta: 1,
+    }));
+    voteTopic.publish(voteBuffer);
+
     res.sendStatus(200);
 });
 
@@ -78,7 +67,7 @@ app.post('/sounds/:soundId/vote', async (req, res) => {
     const { vote, userId } = req.body;
     console.log(`Userid ${userId} votes ${vote} on ${soundId}`);
 
-    const voteDocument = db.doc(`votes/${soundId}_${userId}`);
+    const voteDocument = db.doc(`sounds/${soundId}/votes/${userId}`);
     const existingVote = await voteDocument.get();
     if (existingVote.exists) {
         await voteDocument.update({
@@ -90,11 +79,13 @@ app.post('/sounds/:soundId/vote', async (req, res) => {
         });
     }
     const scoreDelta = vote - (existingVote.exists ? existingVote.get('vote') : 0);
-    const soundDocument = db.doc(`sounds/${soundId}`);
-    const sound = await soundDocument.get();
-    await soundDocument.update({
-        score: sound.get('score') + scoreDelta
-    });
+    if (scoreDelta !== 0) {
+        const buffer = Buffer.from(JSON.stringify({
+            soundId,
+            scoreDelta
+        }));
+        voteTopic.publish(buffer);
+    }
     res.sendStatus(200);
 });
 
